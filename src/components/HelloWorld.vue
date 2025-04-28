a43677ebb935765bce8d85d54da7d50674b5de25
BasdB97 <113598535+BasdB97@users.noreply.github.com>
2025-04-28 12:30:30 +0200

    Project Setup
    Setting up structure and basic styling for the project.
    - Added logo and Favicon
    - Created header with logo
    - Created welcome message

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,42 @@
 <script setup>
 defineProps({
-  msg: {
-    type: String,
-    required: true,
-  },
-})
+	msg: {
+		type: String,
+		required: true,
+	},
+});
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
-  </div>
+	<main>
+		<div class="greetings">
+			<h1>{{ msg }}</h1>
+			<p>
+				RealEstateCare is een applicatie die gebruikt wordt door
+				inspecteurs om rapportages te maken over onderhoudsbehoeften van
+				vastgoed.
+			</p>
+			<em>Deze website is nog in ontwikkeling.</em>
+		</div>
+	</main>
 </template>
 
 <style scoped>
+main {
+	padding: 1rem;
+}
 h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
+	font-weight: 500;
+	font-size: 2.6rem;
+	position: relative;
+	top: -10px;
 }
 
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
+.greetings {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	height: 100%;
 }
 </style>
