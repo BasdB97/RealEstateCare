e3fd55fc4ebb4fd84e80a75d67f2743994ceb946
BasdB97 <113598535+BasdB97@users.noreply.github.com>
2025-07-14 18:07:22 +0200

    Ion Icon fix

--- src/main.js
+++ src/main.js
@@ -1,3 +1,19 @@
+/* Core CSS required for Ionic components to work properly */
+import "@ionic/vue/css/core.css";
+
+/* Basic CSS for apps built with Ionic */
+import "@ionic/vue/css/normalize.css";
+import "@ionic/vue/css/structure.css";
+import "@ionic/vue/css/typography.css";
+
+/* Optional CSS utils that can be commented out */
+import "@ionic/vue/css/padding.css";
+import "@ionic/vue/css/float-elements.css";
+import "@ionic/vue/css/text-alignment.css";
+import "@ionic/vue/css/text-transformation.css";
+import "@ionic/vue/css/flex-utils.css";
+import "@ionic/vue/css/display.css";
+
 import "@ionic/vue/css/ionic.bundle.css";
 
 import "@/assets/base.css";
@@ -9,9 +25,8 @@ import { IonicVue, IonIcon } from "@ionic/vue";
 
 import router from "@/router";
 
-const app = createApp(App).use(IonicVue);
+const app = createApp(App).use(IonicVue).use(router);
 
-// Registreer IonIcon globaal voor alle componenten
-app.component("ion-icon", IonIcon);
-
-app.use(IonIcon).use(router).mount("#app");
+router.isReady().then(() => {
+	app.mount("#app");
+});
