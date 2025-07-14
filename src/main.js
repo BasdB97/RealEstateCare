290f5b6776d8d620edebce6c1c69a540ae876598
BasdB97 <113598535+BasdB97@users.noreply.github.com>
2025-07-14 12:31:09 +0200

    updated IonIcons so it works for deployment

--- src/main.js
+++ src/main.js
@@ -5,9 +5,17 @@ import "@/assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import { IonicVue, IonIcon } from "@ionic/vue";
+import { addIcons } from "ionicons";
+import * as allIcons from "ionicons/icons";
 
 import router from "@/router";
 
-const app = createApp(App).use(IonicVue).use(IonIcon);
+// Register all icons globally
+addIcons(allIcons);
+
+const app = createApp(App).use(IonicVue);
+
+// Register IonIcon as a global component
+app.component("ion-icon", IonIcon);
 
 app.use(router).mount("#app");
