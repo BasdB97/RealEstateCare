ce2a9ab5cbd7b4a220b4942a403a46ff22fb5919
BasdB97 <113598535+BasdB97@users.noreply.github.com>
2025-11-04 09:27:18 +0100

    feat: voeg definitieve styling toe aan footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -16,4 +16,35 @@
 		</div>
 	</footer>
 </template>
-<script setup></script>
+<script setup>
+import { IonIcon } from "@ionic/vue";
+import { home, search, informationCircle } from "ionicons/icons";
+</script>
+
+<style scoped>
+.footer-link {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	text-decoration: none;
+	color: white;
+	font-size: 14px;
+	line-height: 1.4286;
+	@media (width >= 640px) {
+		font-size: 16px;
+		line-height: 1.5;
+	}
+	@media (width >= 768px) {
+		font-size: 20px;
+		line-height: 1.4;
+	}
+}
+.footer-link:active {
+	color: #d1d5db;
+	transition-property: color, background-color, border-color, outline-color, text-decoration-color,
+		fill, stroke;
+	transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+	transition-duration: 200ms;
+	scale: 1.05;
+}
+</style>
