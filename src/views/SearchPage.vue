d322ee72328e7e8e414a2c88bfc256c86d845fb3
BasdB97 <113598535+BasdB97@users.noreply.github.com>
2025-11-20 12:54:39 +0100

    feat: Update SearchPage layout and functionality
    - Changed the page title from "Informatie" to "Zoeken" for better context.
    - Added an IonSearchbar for searching tasks and documents.
    - Introduced an IonList to display search results with a placeholder message.
    - Imported necessary Ionic components to enhance the user interface.

--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -1,11 +1,20 @@
 <template>
 	<BaseLayout>
 		<div class="p-4 max-w-2xl mx-auto">
-			<h1 class="text-3xl font-bold text-primarybg dark:text-white mb-6">Informatie</h1>
+			<h1 class="text-3xl font-bold text-primarybg dark:text-white mb-6">Zoeken</h1>
+			<IonSearchbar placeholder="Zoek naar taken, documenten etc." class="dark:text-white mb-4" />
+			<IonList
+				class="m-2 p-2 border-2 border-primarybg dark:border-slate-600 rounded-lg shadow-md space-y-2">
+				<IonItem lines="full">
+					<IonLabel>
+						<span class="font-semibold dark:text-white">Resultaten komen hier</span>
+					</IonLabel>
+				</IonItem>
+			</IonList>
 		</div>
 	</BaseLayout>
 </template>
 
-<script setup></script>
-
-<style scoped></style>
+<script setup>
+import { IonList, IonItem, IonLabel, IonSearchbar } from "@ionic/vue";
+</script>
